d654537c8b79750c9cd01f2ecacc46b8cade90c9
aldopariani <aldo.pariani3@gmail.com>
2022-10-02 13:16:45 -0300

    Desafíate - Entrega 4

--- js/product-info.js
+++ js/product-info.js
@@ -35,15 +35,35 @@ fetch(URLS)
             </div>
             <div class"imagenes-ilustrativas">
                 <p><b>Imagenes ilustrativas</b></p>
-                <div id="imagenes-info">
-                    <img src="${prod.images[0]}" class="img-info"></img>
-                    <img src="${prod.images[1]}" class="img-info"></img>
-                    <img src="${prod.images[2]}" class="img-info"></img>
-                    <img src="${prod.images[3]}" class="img-info"></img>
+                <div id="imagenes-info">                   
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="${prod.images[0]}" class="d-block img-info">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="${prod.images[1]}" class="d-block img-info">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="${prod.images[2]}" class="d-block img-info">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="${prod.images[3]}" class="d-block img-info">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>                    
                 </div>
             </div>
         </div>       
-    `;
+    `;   
     let ProdRel = producto.relatedProducts;
     console.log(ProdRel);
     for (let Rel of ProdRel) {
@@ -67,10 +87,6 @@ fetch(URLS)
         });   
     });
 });
-
-
-
-
 
 
 fetch(URL_comentarios)
