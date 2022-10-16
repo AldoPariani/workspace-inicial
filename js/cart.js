e7f94797d1a1e401d3c946a27c91658dd0b890bd
aldopariani <aldo.pariani3@gmail.com>
2022-10-16 15:55:23 -0300

    Entrega 5 - Termianda

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,43 @@
 const CART_INFO = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
 
+let contenedorCarrito = document.getElementById('info-cart');
+
 fetch(CART_INFO)
 .then(respuesta => respuesta.json())
 .then(datos => {
     console.log(datos);
+    let preCarrito = datos.articles[0];
+    console.log(preCarrito);
+    contenedorCarrito.innerHTML += `
+    <div class="col-2">
+        <img src="${preCarrito.image}" alt="" id="img-preCarr">
+    </div>
+    <div class="col-2">
+        <p>
+            ${preCarrito.name}
+        </p>
+    </div>
+    <div class="col-2">
+        <p>
+            ${preCarrito.currency} ${preCarrito.unitCost}
+        </p>
+    </div>
+    <div class="col-2">
+        <input type="number" name="cantidad" id="cantidad" min="1" value="${preCarrito.count}">
+    </div>
+    <div class="col-3">
+        <p  id="sub-Tot">
+            ${preCarrito.currency} ${preCarrito.unitCost*preCarrito.count} 
+        </p>
+    </div>
+    `;
+    let cant = document.getElementById('cantidad');
+    cant.addEventListener('input', (e) =>{
+        let precioUnit = preCarrito.unitCost;
+        let subtotal = precioUnit * cant.value;
+        console.log(subtotal); 
+        document.getElementById('sub-Tot').innerHTML = `
+                ${preCarrito.currency} ${subtotal}
+        `;  
+    });
 });
