ae52e4273dacca47d31f878d0b0ff641704ce303
aldopariani <aldo.pariani3@gmail.com>
2022-10-28 19:59:59 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -76,25 +76,3 @@ fetch(CART_INFO)
         </div>
     `;
 });
-
-// (function () {
-//     'use strict'
-//     var forms = document.querySelectorAll('.needs-validation')
-  
-//     Array.prototype.slice.call(forms)
-//       .forEach(function (form) {
-//         form.addEventListener('submit', function (event) {
-//             verificarModal();
-//             if (!form.checkValidity()) {
-//             event.preventDefault()
-//             event.stopPropagation()
-//             }
-//             form.classList.add('was-validated')
-//         }, false)
-//     })
-// })()
-function modalver() {
-    if (document.getElementById('tarjeta').checkValidity() === true) {
-        document.getElementById('num-tarj').classList.remove('filtro');
-    }
-}
